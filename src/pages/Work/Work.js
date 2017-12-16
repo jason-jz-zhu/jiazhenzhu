9eeeaf88ef246cafa44e625b9bb195bccaba63fc
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2017-12-16 00:55:59 -0500

    add abstract and cards on the work page

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -4,70 +4,93 @@ import './Work.css';
 class Work extends Component {
 
   render() {
+    const abstract = (
+      <div className="WorkAbstractWrapper">
+        <div className="textContainer">
+          <h1 className="leftHeader">
+            <strong className="jiazhen">
+              Project
+            </strong>
+          </h1>
+          <h2 className="rightHeader">
+            MY PERSONAL EXPERIMENTS & PROFESSIONAL WORK
+          </h2>
+        </div>
+      </div>
+    )
     return (
-      <div className="cardsWrapper">
-        <div className="cards">
-        
-        
-        
-        
-          <div className="cardWrapper">
-            <div className="card">
-              
+      <div>
+        {abstract}
+        <div className="cardsWrapper">
+          <div className="cards">
+
+
+            <div className="cardWrapper">
+
               <a href="" className="card">
-                <div className="thumb"></div>
-                <article>
-                  <h1>Post title</h1>
-                  <span>Author</span>
-                </article>
+                <div className="thumb" style={{backgroundImage: "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg)"}}>
+                  <div className="titleContent">
+                    <h3>10 inspiring photos</h3>
+                  </div>
+                  <div className="cardInfo">
+                    D3
+                  </div>
+                  <div className="colorOverlay"></div>
+                </div>
               </a>
-          
+
             </div>
-          </div>
-          
-          <div className="cardWrapper">
-            <div className="card">
-              
+
+            <div className="cardWrapper">
+
               <a href="" className="card">
-                <div className="thumb"></div>
-                <article>
-                  <h1>Post title</h1>
-                  <span>Author</span>
-                </article>
+                <div className="thumb" style={{backgroundImage: "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg)"}}>
+                  <div className="titleContent">
+                    <h3>10 inspiring photos</h3>
+                  </div>
+                  <div className="cardInfo">
+                    D3
+                  </div>
+                  <div className="colorOverlay"></div>
+                </div>
               </a>
-          
+
             </div>
-          </div>
-          
-          <div className="cardWrapper">
-            <div className="card">
-              
+
+            <div className="cardWrapper">
+
               <a href="" className="card">
-                <div className="thumb"></div>
-                <article>
-                  <h1>Post title</h1>
-                  <span>Author</span>
-                </article>
+                <div className="thumb" style={{backgroundImage: "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg)"}}>
+                  <div className="titleContent">
+                    <h3>10 inspiring photos</h3>
+                  </div>
+                  <div className="cardInfo">
+                    D3
+                  </div>
+                  <div className="colorOverlay"></div>
+                </div>
               </a>
-          
+
             </div>
-          </div>
-          
-          <div className="cardWrapper">
-            <div className="card">
-              
+
+            <div className="cardWrapper">
+
               <a href="" className="card">
-                <div className="thumb"></div>
-                <article>
-                  <h1>Post title</h1>
-                  <span>Author</span>
-                </article>
+                <div className="thumb" style={{backgroundImage: "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg)"}}>
+                  <div className="titleContent">
+                    <h3>10 inspiring photos</h3>
+                  </div>
+                  <div className="cardInfo">
+                    D3
+                  </div>
+                  <div className="colorOverlay"></div>
+                </div>
               </a>
-          
+
             </div>
+
+
           </div>
-          
-          
         </div>
       </div>
     );
